02bed2f357b33a1511853c50bcc6d5b86814e8a4
Dejan Brinker <dejan@mia3.com>
2020-10-19 15:04:45 +0200

    Add Glightbox dependency + cleanup TCA for header & image + cleanup native content elements

--- packages/template/Configuration/TCA/Overrides/tt_content/image.php
+++ packages/template/Configuration/TCA/Overrides/tt_content/image.php
@@ -100,3 +100,25 @@ $GLOBALS['TCA']['tt_content']['types']['image']['columnsOverrides']['image']['co
         ],
     ],
 ];
+
+$GLOBALS['TCA']['tt_content']['types']['image']['showitem'] = "
+    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+     --palette--;;general,
+    --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.images,
+     image,
+     --palette--;;imagelinks,
+     --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+     --palette--;;frames,
+     --palette--;;appearanceLinks,
+    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+     --palette--;;language,
+    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+     --palette--;;hidden,
+     --palette--;;access,
+    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
+    --div--;LLL:EXT:core/Resources/Private/Language/locallang_tca.xlf:sys_category.tabs.category,
+     categories,
+    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+     rowDescription,
+    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended";
+

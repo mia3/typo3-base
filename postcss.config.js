8f6eebbd31c1a17045ce2ca26a62819c61b4e6d6
Tobias <tobias@mia3.com>
2020-10-28 11:33:22 +0100

    Before CSS changes

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,26 @@
 module.exports = {
   plugins: {
+    // include whatever plugins you want
+    // but make sure you install these via yarn or npm!
     'postcss-easy-import': {},
-    'postcss-css-variables': {
-      options: {
-        preserve: true
-      }
-    },
-    'postcss-custom-media': {
-      extensions: {
-        '--screen-mobile': '(min-width: 480px)',
-        '--screen-tablet': '(min-width: 768px)',
-        '--screen-tablet-landscape': '(min-width: 992px)',
-        '--screen-desktop': '(min-width: 1200px)'
-      }
-    },
-    'cssnext': {},
-    'autoprefixer': {}
+
+    // add browserslist config to package.json (see below)
+    // autoprefixer: {},
+
+
+    // 'postcss-css-variables': {
+    //   options: {
+    //     preserve: true
+    //   }
+    // },
+    // 'postcss-custom-media': {
+    //   extensions: {
+    //     '--screen-mobile': '(min-width: 480px)',
+    //     '--screen-tablet': '(min-width: 768px)',
+    //     '--screen-tablet-landscape': '(min-width: 992px)',
+    //     '--screen-desktop': '(min-width: 1200px)'
+    //   }
+    // },
+    // 'cssnext': {},
   }
 };

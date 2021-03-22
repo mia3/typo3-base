8235fa0958f750385eb7d7dfa3caaa46a46e9d2b
Patrick Narmont <patrick@mia3.com>
2021-03-22 10:42:30 +0100

    Update headline to separate semantic and style

--- packages/template/ext_tables.sql
+++ packages/template/ext_tables.sql
@@ -1,12 +1,11 @@
 create table tt_content
 (
-    header_style          varchar(30)         default '0' not null,
+    header_style          tinyint             default '0' not null,
     header_color          tinyint             default '0' not null,
-    header_semantic       tinyint(4) unsigned DEFAULT '1' NOT NULL,
     tx_template_container tinytext            default ''  not null,
 );
 
 create table sys_file_reference
 (
-    stretch_across_content tinyint(4) unsigned default '0' not null
+    stretch_across_content tinyint(4) unsigned default '0' not null,
 );

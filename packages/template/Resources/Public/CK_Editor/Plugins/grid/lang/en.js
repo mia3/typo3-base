7cc3dfb4d1fad5aae34901cdb7748d53886f5df4
Patrick Narmont <patrick@mia3.com>
2021-03-22 10:44:59 +0100

    Remove grid from rte editor

--- packages/template/Resources/Public/CK_Editor/Plugins/grid/lang/en.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-CKEDITOR.plugins.setLang("columns", "en", {
-    numCols: "Select number of columns",
-    info: "Info",
-    createGrid: "Create a grid",
-    editGrid: "Edit grid",
-    numColsError: "Please select number of columns."
-});

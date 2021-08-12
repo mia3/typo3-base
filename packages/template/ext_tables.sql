d13afd3dd00cef71e6c3db3d0c6f0193fab9c350
Frederic Hesse <frederic@mia3.com>
2021-08-13 00:19:25 +0200

    [Task] TCA, HTML, CSS Clean Up

--- packages/template/ext_tables.sql
+++ packages/template/ext_tables.sql
@@ -3,7 +3,6 @@ create table tt_content
     header_style          tinyint             default '0' not null,
     header_color          tinyint             default '0' not null,
     tx_template_container tinytext            default ''  not null,
-    text_columns          tinyint(4)          default '0' not null,
 );
 
 create table sys_file_reference
